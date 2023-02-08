0d74fe7e72fbcf13ad2bb49cab5810dcf8afe96e
NATEE <natee@iavar.one>
2023-02-08 17:21:23 +0100

    changes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,30 @@
 import winston, { loggers, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import treeify from 'object-treeify';
+
+const dailyRotateFileTransport = new DailyRotateFile({
+  dirname: 'logs',
+  filename: 'kit-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '14'
+});
+
+const treeFormat = winston.format((info) => {
+  
+  info.message = '\n\n' + treeify(info);
+  
+  return info;
+})();
 
 const level = 'info';
 
 const consoleTrans = new transports.Console({
   level,
-  format: format.simple(),
+  format: format.combine(
+    treeFormat,
+    format.printf(info => info.message)
+  )
 });
 winston.add(consoleTrans);
 
@@ -15,7 +35,7 @@ const defaultLogger = loggers.add('default', {
     format.json()
   ),
   defaultMeta: {},
-  transports: [consoleTrans],
+  transports: [consoleTrans, dailyRotateFileTransport],
 });
 
 const serverLogger = defaultLogger.child({
@@ -27,23 +47,35 @@ const toolsLogger = serverLogger.child({
   domain: `server.tools`,
   toolName: 'myToolName',
 });
+const formLogger = toolsLogger.child({
+  ...toolsLogger.defaultMeta,
+  domain: `server.tools.form`,
+  formName: 'myFormName',
+});
 const canvasLogger = toolsLogger.child({
   ...toolsLogger.defaultMeta,
   domain: `server.tools.canvas`,
   canvasName: 'myCanvasName',
 });
+const canvasImageLogger = canvasLogger.child({
+  ...canvasLogger.defaultMeta,
+  domain: `server.tools.canvas.images`,
+  imageName: 'myImageName',
+});
 
 loggers.loggers.set('server', serverLogger);
 loggers.loggers.set('server.tools', toolsLogger);
+loggers.loggers.set('server.tools.form', formLogger);
 loggers.loggers.set('server.tools.canvas', canvasLogger);
+loggers.loggers.set('server.tools.canvas.images', canvasImageLogger);
 
-export async function handle({ event, resolve }) {
-  if (event.url.pathname.startsWith('/custom')) {
-    return new Response('custom response');
-  }
-	serverLogger.info('handle', { event });
+// export async function handle({ event, resolve }) {
+//   if (event.url.pathname.startsWith('/custom')) {
+//     return new Response('custom response');
+//   }
+// 	serverLogger.info('handle', { event });
  
-  const response = await resolve(event);
+//   const response = await resolve(event);
 
-  return response;
-}
+//   return response;
+// }
